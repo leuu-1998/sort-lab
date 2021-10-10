34b2cfb31f0cf5da3c16fbdab7b46dd2a22cbde1
leuu-1998 <lmoinah@uni.pe>
2021-10-09 23:26:30 -0500

    process agregado a controlador

--- controlador.vhd
+++ controlador.vhd
@@ -1,3 +1,5 @@
+--se implementa la maquina de estados FSM-D
+-- estados inicial de reset S0 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -16,9 +18,23 @@ end controlador;
 --  Architecture Body
 architecture behav of controlador is
 -- you may need to define state type signals and other signals
+	type STATE_TYPE is (S0,S1,S2,S3,S4);
+	--initial state
+	signal state: STATE_TYPE := S0;
 
 begin
 -- use the two process model for the FSM
 -- outputs to the datapath should be Moore type to avoid timing issues
+	process(clock,resetn)
+	begin
+		if (resetn = '1') then
+			state <= S0;
+		end if;
+	end process;
 
+--process 2
+	process(state)
+	begin
+		
+	end process;
 end behav;
