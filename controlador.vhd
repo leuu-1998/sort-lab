14739929e6381d8ecd5dc69fdaef9296e0ef7b4b
leuu-1998 <lmoinah@uni.pe>
2021-10-12 23:50:42 -0500

    agregamos los estados al controlador

--- controlador.vhd
+++ controlador.vhd
@@ -20,7 +20,7 @@ architecture behav of controlador is
 -- you may need to define state type signals and other signals
 	type STATE_TYPE is (S0,S1,S2,S3,S4);
 	--initial state
-	signal state: STATE_TYPE := S0;
+	signal state, next_state : STATE_TYPE;
 
 begin
 -- use the two process model for the FSM
@@ -37,7 +37,58 @@ begin
 
 --process 2
 	process(state)
+
 	begin
-		
+		case state is
+			when S0 =>
+				control <='000';
+				if (go='0') then
+					next_state <= S1;
+				end if;
+			
+			when S1 =>
+				control <= '001';
+				if(go='1') then 
+					next_state <= S2;
+				end if;
+			
+			when S2 =>
+			-- se asgina que i y j son cero
+				control <= '010';
+				next_state <= S3;
+			
+			when S3 =>
+				control <= '011';
+				next_state <= S4;
+				
+			when S4 =>
+				control <= '100';
+				next_state <= S5;
+
+			when S5 =>
+				control <= '101';
+				--preguntando j para pasar a otro estad
+				if(j='111') then 
+					next_state <= S6;
+				else 
+					next_state <= S4;
+				end if;
+
+			when S6 =>
+				control <= '110';
+				--preguntamos para i
+				-- 14
+				if(i = '110') then 
+					next_state <= S7;
+				else
+					next_state <= S2;
+				end if;
+
+			when S7 =>
+				control <= '111';
+				--queda en duda si al S0 o al S1
+				next_state <= S0;
+		end case;
+
 	end process;
 end behav;
