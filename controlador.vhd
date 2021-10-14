49b2a7e53bd19bf10ee524117d21097f31d9cb08
leuu-1998 <lmoinah@uni.pe>
2021-10-13 22:26:08 -0500

    agregamos las señales internas cj, ci y asginamos las salidas, casi listo para pruebas

--- controlador.vhd
+++ controlador.vhd
@@ -21,7 +21,7 @@ architecture behav of controlador is
 	type STATE_TYPE is (S0,S1,S2,S3,S4);
 	--initial state
 	signal state, next_state : STATE_TYPE;
-
+	signal cj, ci : std_logic_vector(2 downto 0) := (others => '0'); 
 begin
 -- use the two process model for the FSM
 -- outputs to the datapath should be Moore type to avoid timing issues
@@ -32,22 +32,30 @@ begin
 	-- reset asincrono 2
 		if (resetn = '1') then
 			state <= S0;
-		end if;
+			-- cuando se usa el reset cj y ci se reinician en 0
+			cj <= (others '0');
+			ci <= (others '0');
+			control <= '000';
+			done <='0';
+		elsif (rising_edge(clock) ) then
+			state <= next_state;
+		end if ;
 	end process;
 
 --process 2
 	process(state)
-
 	begin
 		case state is
 			when S0 =>
 				control <='000';
+				done <= '0';
 				if (go='0') then
 					next_state <= S1;
 				end if;
 			
 			when S1 =>
 				control <= '001';
+				done <= '0';
 				if(go='1') then 
 					next_state <= S2;
 				end if;
@@ -55,40 +63,54 @@ begin
 			when S2 =>
 			-- se asgina que i y j son cero
 				control <= '010';
+				done <= '0';
 				next_state <= S3;
 			
 			when S3 =>
 				control <= '011';
+				done <= '0';
 				next_state <= S4;
 				
 			when S4 =>
 				control <= '100';
-				next_state <= S5;
-
+				done <= '0';
+				--preguntando j para pasar a otro estad
+				if(cj='111') then 
+					next_state <= S5;
+				else 
+					next_state <= S3;
+				end if;
 			when S5 =>
 				control <= '101';
-				--preguntando j para pasar a otro estad
-				if(j='111') then 
+				done <= '0';
+				--preguntamos para i
+				-- 14
+				if(ci = '110') then 
+					next_state <= S7;
+				else
 					next_state <= S6;
-				else 
-					next_state <= S4;
 				end if;
 
 			when S6 =>
 				control <= '110';
-				--preguntamos para i
-				-- 14
-				if(i = '110') then 
-					next_state <= S7;
-				else
-					next_state <= S2;
-				end if;
+				done <= '0';
+				next_state <= S2;
 
 			when S7 =>
 				control <= '111';
+				done <= '1';
 				--queda en duda si al S0 o al S1
 				next_state <= S0;
 		end case;
-
-	end process;
+		-- aqui se agregan los outputs
+		-- i , j , done
+		-- i , j = 0 , S0 , S1 , S7
+		-- cj, ci
+		cj <= ci when state = S2 ;
+		cj <= cj + 1  when state = S3;
+		ci <= ci + 1 when state = S6;
+		end process;
+		-- se asigna las señales internas a las salidas i, j
+		i <= ci;
+		j <= cj;
 end behav;
