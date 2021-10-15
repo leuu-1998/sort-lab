62a90aab3fceaf8d1c5deeb6a11fc66d7186e107
sheylaloz <80723760+sheylaloz@users.noreply.github.com>
2021-10-14 20:39:31 -0500

    señales ci_n y cj_n, reducí un estado

--- controlador.vhd
+++ controlador.vhd
@@ -18,10 +18,11 @@ end controlador;
 --  Architecture Body
 architecture behav of controlador is
 -- you may need to define state type signals and other signals
-	type STATE_TYPE is (S0,S1,S2,S3,S4,S5,S6,S7);
+	type STATE_TYPE is (S0,S1,S2,S3,S4,S5,S7);
 	--initial state
 	signal state, next_state : STATE_TYPE;
-	signal cj, ci : std_logic_vector(2 downto 0) := (others => '0'); 
+	signal cj, ci : unsigned(2 downto 0) := (others => '0'); 
+	signal cj_n, ci_n : unsigned(2 downto 0) := (others => '0'); 
 begin
 -- use the two process model for the FSM
 -- outputs to the datapath should be Moore type to avoid timing issues
@@ -34,22 +35,29 @@ begin
 			-- cuando se usa el reset cj y ci se reinician en 0
 			cj <= (others => '0');
 			ci <= (others => '0');
-			control <= "000";
-			done <='0';
+			--control <= "000";
+			--done <='0';
 		elsif (rising_edge(clock) ) then
 			state <= next_state;
+			ci<=ci_n;
+			cj<=cj_n;
 		end if ;
 	end process;
 
 --process 2
 	process(state,go,ci,cj)
 	begin
+
+		ci_n <= ci;
+		cj_n <= cj;
+		next_state <= state;
+		
 		case state is
 			when S0 =>
-				control <= "000";
+				control <= (others => '0'); 
 				done <= '0';
-				ci <= "000";
-				cj <= "000";
+				ci_n <= "000";
+				cj_n <= "000";
 				if (go='0') then
 					next_state <= S1;
 				end if;
@@ -57,8 +65,8 @@ begin
 			when S1 =>
 				control <= "001";
 				done <= '0';
-				ci <= "000";
-				cj <= "000";
+				ci_n <= "000";
+				cj_n <= "000";
 				if(go='1') then 
 					next_state <= S2;
 				end if;
@@ -68,22 +76,22 @@ begin
 				control <= "010";
 				done <= '0';
 				-- acciones
-				cj <= ci;
+				cj_n <= ci;
 				--ci no cambia
-				ci <= ci;
+				ci_n <= ci;
 				next_state <= S3;
 			
 			when S3 =>
 				control <= "011";
 				done <= '0';
-				cj <= std_logic_vector(unsigned(cj) + 1);
-				ci <= ci;
+				cj_n <= cj + 1;
+				ci_n<= ci;
 				next_state <= S4;
 			when S4 =>
 				control <= "100";
 				done <= '0';
-				ci<=ci;
-				cj<=cj;
+				ci_n<=ci;
+				cj_n<=cj;
 				--preguntando j para pasar a otro estad
 				if(cj="111") then 
 					next_state <= S5;
@@ -93,36 +101,22 @@ begin
 			when S5 =>
 				control <= "101";
 				done <= '0';
-				ci<=ci;
-				cj<=cj;
+				ci_n<=ci + 1;
+				cj_n<=cj;
 				--preguntamos para i
 				if(ci = "110") then 
 					next_state <= S7;
 				else
-					next_state <= S6;
+					next_state <= S2;
 				end if;
 
-			when S6 =>
-				control <= "110";
-				done <= '0';
-				ci <= std_logic_vector(unsigned(ci) + 1);
-				cj <= cj;
-				next_state <= S2;
 
 			when S7 =>
 				control <= "111";
 				done <= '1';
-				ci <= "000";
-				cj <= "000";
+				ci_n <= "000";
+				cj_n <= "000";
 				next_state <= S0;
-			
-			when others => 
-				control <= "000";
-				done <= '0';
-				ci <= "000";
-				cj <= "000";
-				next_state <= state;
-				
 				
 		end case;
 		-- aqui se agregan los outputs
@@ -131,6 +125,6 @@ begin
 		-- cj, ci
 		end process;
 		-- se asigna las señales internas a las salidas i, j
-		i <= ci;
-		j <= cj;
+		i <= std_logic_vector(ci);
+		j <= std_logic_vector(cj);
 end behav;
